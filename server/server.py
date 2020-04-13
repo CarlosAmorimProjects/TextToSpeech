77c567d99fedc738750ae54e1ddb3bae2d625524
djkajo <carlosserraamorim@gmail.com>
2020-04-13 16:19:34 +0100

    pull request 2

--- server/server.py
+++ server/server.py
@@ -1,6 +1,7 @@
 # server.py
 from flask import Flask, render_template, request
 import speak
+import url_for
 
 # text = "AI AI minha machadinha !!"
 # lang = "pt"
@@ -13,13 +14,13 @@ def index():
 @app.route("/hello", methods=["GET","POST"])
 def hello():
     text=''
-    lang='pt'
+    lang=''
     if request.method == "GET":
         text = request.form["text"]
         lang = request.form["lang"]
         print(text)
         speak.get_speak(text,lang)
-        return flask.url_for("hello", filename="mp3_path")
+        return url_for ("static", filename="mp3_path")
 
 if __name__ == "__main__":
     app.run()
