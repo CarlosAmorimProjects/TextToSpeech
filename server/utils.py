62dc0c86bf62363ebd584def0846140343a89968
djkajo <carlosserraamorim@gmail.com>
2020-05-16 16:05:42 +0100

    creates audiofiles if does not exist

--- server/utils.py
+++ server/utils.py
@@ -5,6 +5,8 @@ import glob
 
 def random_tts_filename ():
     filetext = str(uuid.uuid4())
+    if not os.path.exists("../static/build/static/audiofiles/"):
+        os.makedirs("../static/build/static/audiofiles/")
     filename = "../static/build/static/audiofiles/" + filetext + ".mp3"
     return filename
 
